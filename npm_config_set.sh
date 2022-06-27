621e7f1da8b0b44587022b156fb6b59aabedb78c
Gianluca Casati <fibo@users.noreply.github.com>
2022-06-27 15:01:55 +0200

    update to latest npm cli

--- npm_config_set.sh
+++ npm_config_set.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-npm config set init.author.name "Gianluca Casati"
-npm config set init.author.url http://fibo.github.io
-npm config set init.license MIT
-npm config set init.version 0.0.0
+npm config set init-author-name "Gianluca Casati"
+npm config set init-author-url http://fibo.github.io
+npm config set init-license MIT
+npm config set init-version 0.0.0
